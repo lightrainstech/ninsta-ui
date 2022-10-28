0850c6328b83131cea8488124b7719c82b71546a
Sibiraj PR <sibiraj@lightrains.com>
2022-10-28 14:30:06 +0530

    Remove connect wallet

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -65,7 +65,6 @@ const Menu = props => {
                     </Link>
                   )
                 })}
-                <ConnectWallet />
               </div>
             </div>
           )}
