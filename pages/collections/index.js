105cae3cfdc3b30da78d830968e231de3563c608
niksmac <nikhil@lightrains.com>
2022-11-09 11:55:36 +0530

    Add collections page

---
+++ pages/collections/index.js
@@ -0,0 +1,29 @@
+import Footer from '../../components/Footer'
+import Latest from './Latest'
+import Masthead from './Masthead'
+import Menu from '../../components/Menu'
+import { NextSeo } from 'next-seo'
+
+export default function Collections() {
+  return (
+    <>
+      <NextSeo
+        title="How to add Free NFT to Instagram"
+        description="How to add Free NFT to your Instagram account"
+      />
+      <Menu />
+      <section className="container py-20">
+        <h1 className="text-6xl font-bold leading-relaxed text-center font-serif">
+          Ninsta Collections
+        </h1>
+        <h2 className="text-3xl leading-relaxed text-center text-medium mt-4 mb-12 w-full md:w-[700px] mx-auto">
+          Ninsta Collections are the best way for you as a creator to enter the
+          world of Digital Collectibles.
+        </h2>
+        <Latest />
+        <Masthead />
+      </section>
+      <Footer />
+    </>
+  )
+}
