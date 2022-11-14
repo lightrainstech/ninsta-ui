e70649caf5dcd9b87c2cec28167cbdea2f6b75d4
niksmac <nikhil@lightrains.com>
2022-11-14 19:19:55 +0530

    Add non-minted item

--- components/Mint/PreviousMints.js
+++ components/Mint/PreviousMints.js
@@ -3,11 +3,33 @@ import { formatDate, truncate } from '../../utils'
 
 import Image from 'next/image'
 import Link from 'next/link'
+import { RiImageEditLine } from 'react-icons/ri'
 import { getAssets } from '../../actions'
 import getConfig from 'next/config'
 import { useSelector } from 'react-redux'
 
 const { publicRuntimeConfig } = getConfig()
+
+const EmptyCard = ({ asset }) => {
+  return (
+    <div className="bg-zinc-800 flex flex-col p-3 gap-3 rounded-md cursor-wait relative">
+      <span className="absolute right-0 bg-yellow-300 h-8 opacity-100 z-50 top-0 rounded-tr-md rounded-bl-md px-3 text-black">
+        Getting Ready...
+      </span>
+      <div className="h-[260px] w-[270px] animate-pulse bg-gray-900 flex flex-col items-center justify-center">
+        <RiImageEditLine size={40} />
+      </div>
+      <strong className="text-xl">{asset.title}</strong>
+      <div class="h-3 bg-slate-700 rounded animate-pulse mt-2"></div>
+      <div class="h-3 bg-slate-700 rounded animate-pulse mt-2"></div>
+      <div class="h-3 bg-slate-700 rounded animate-pulse mt-2"></div>
+      <div class="h-3 bg-slate-700 rounded animate-pulse mt-2"></div>
+      <div class="h-3 bg-slate-700 rounded animate-pulse mt-2"></div>
+      <div class="h-3 bg-slate-700 rounded animate-pulse mt-2"></div>
+      <div class="h-3 bg-slate-700 rounded animate-pulse mt-2"></div>
+    </div>
+  )
+}
 
 const DCCard = ({ asset }) => {
   return (
@@ -89,15 +111,21 @@ const PreviousMints = () => {
     getAllAssets()
   }, [])
 
+  console.log(assets)
+
   return (
     <div className="py-6 overflow-hidden">
       <h3 className="text-xl mb-3">Your previous mints</h3>
 
       <div className="w-[600px] md:w-[900px] overflow-scroll">
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          {assets.map((e, i) => (
-            <DCCard key={i} asset={e} />
-          ))}
+          {assets.map((e, i) => {
+            return e.tokenId ? (
+              <DCCard key={i} asset={e} />
+            ) : (
+              <EmptyCard key={i} asset={e} />
+            )
+          })}
         </div>
       </div>
     </div>
