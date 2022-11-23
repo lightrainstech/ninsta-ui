81171b14e54015bd5e2d8ed7347ac6349b1bec6f
niksmac <nikhil@lightrains.com>
2022-11-23 18:00:24 +0530

    Change safari pinned tab color

--- components/Favicons.jsx
+++ components/Favicons.jsx
@@ -6,25 +6,25 @@ function Favicons() {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicons/apple-touch-icon.png?v=22"
+        href="/favicons/apple-touch-icon.png?v=23"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicons/favicon-32x32.png?v=22"
+        href="/favicons/favicon-32x32.png?v=23"
       />
-      <link rel="manifest" href="/favicons/site.webmanifest?v=22" />
+      <link rel="manifest" href="/favicons/site.webmanifest?v=23" />
       <link
         rel="mask-icon"
-        href="/favicons/safari-pinned-tab.svg?v=22"
-        color="#BAF247"
+        href="/favicons/safari-pinned-tab.svg?v=23"
+        color="#100e1a"
       />
-      <link rel="shortcut icon" href="/favicons/favicon.ico?v=22" />
+      <link rel="shortcut icon" href="/favicons/favicon.ico?v=23" />
       <meta name="msapplication-TileColor" content="#111111" />
       <meta
         name="msapplication-config"
-        content="/favicons/browserconfig.xml?v=22"
+        content="/favicons/browserconfig.xml?v=23"
       />
       <meta name="theme-color" content="#100E1A" />
     </>
