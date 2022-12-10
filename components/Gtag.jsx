dbdd21f10e8f611006f75a0e7938b373fdcba28d
niksmac <nikhil@lightrains.com>
2022-12-10 20:18:53 +0530

    Add gtag

--- components/Gtag.jsx
+++ components/Gtag.jsx
@@ -2,12 +2,18 @@ import Script from 'next/script'
 
 function Gtag() {
   return (
-    <Script id="google-analytics" strategy="afterInteractive">
-      {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NGFFKC2');`}
-    </Script>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-2YMN4CSY6P"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-2YMN4CSY6P');`}
+      </Script>
+    </>
   )
 }
 
